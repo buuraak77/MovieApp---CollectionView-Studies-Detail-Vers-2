9dbae9cfcf75ef9ec8bed4a3542aa8896dd73cf7
buuraak77 <brkylmz8@hotmail.com>
2022-07-19 16:03:36 +0300

    Initial Commit

---
+++ MovieApp - CollectionView Studies(Detail)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp - CollectionView Studies(Detail)
+//
+//  Created by Burak Yılmaz on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
